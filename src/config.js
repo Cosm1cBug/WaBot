2d54b0b9e48ed513d424bb21bcc95050745db5fc
COSMICBUG <54317137+Cosm1cBug@users.noreply.github.com>
2024-10-27 19:04:38 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const config = {
     omdb : {
         url: 'https://www.omdbapi.com/',
-        key: '5fa0312b'
+        key: ''
     },
     options: {
         antiCall: true, //If true, rejects calls. If false do nothing.
         aboutUpdate: "", //leave empty to do nothing.
-        BAdmin: [919746824845], //Give number for administration.
+        BAdmin: [123456789], //Give number for administration.
         prefix: /^[$]/gi, //Msgs starting with '.' considered as commands.
         ReadMessages: true, //set true to read messages
         ReadStatus: true, //set true to read status/stories
@@ -21,26 +21,26 @@ const config = {
         packAuthor: "🗦COSMICBUG🗧",
     },
     WApp: {
-        MovGrp: "120363028891755196@g.us", //Mov grp
-        UserLogs: "120363245835527889@g.us", //For saving user data.
-        BotLogs: "120363032238569810@g.us", //Give chat/group id to send logs. 
-        CallLogs: "120363291777826885@g.us",
-        Syslogs: "120363273008568064@g.us",
-        GrpUpdates: "120363032238569810@g.us",
-        ErrLogs: "120363292760134394@g.us",
+        MovGrp: "1234567@g.us", //Mov grp
+        UserLogs: "12345678@g.us", //For saving user data.
+        BotLogs: "12345678@g.us", //Give chat/group id to send logs. 
+        CallLogs: "12345678@g.us",
+        Syslogs: "1234567@g.us",
+        GrpUpdates: "1234567@g.us",
+        ErrLogs: "1234567@g.us",
     },
     Tele: {
-        userID: '295659233',
-        appID: '16618822',
-        apiHash: 'b30f57c233df3daa10ec47bede728715',
-        channelID: '2121409821', //To save image, audio, video & documents from WhatsApp.
-        groupID: '2053369164', //To send messages to WhatsApp.
-        botID: '6773630491',
+        userID: '123456',
+        appID: '1234567',
+        apiHash: 'sdfg2345gh234v',
+        channelID: '1234567', //To save image, audio, video & documents from WhatsApp.
+        groupID: '1234567', //To send messages to WhatsApp.
+        botID: '123456',
         botUserName: '@thebuggybot',
-        botToken: '6773630491:AAHBkTWXl561udd0p19yQ-tlcLbT8rEpqoc',
+        botToken: '6773630491:12sdfgh23fgh-tlcLbT8rEpqoc',
     },
     MongoDB: {
-        URI: "mongodb://localhost:27017/BUGGY"
+        URI: "mongodb://localhost:27017/"
     },
     WorkMode: {
         public: true //set true to make public
